c0827649428d641710d4521e8d5feb5f1c22e956
msapunov <matvey.sapunov@gmail.com>
2018-11-30 10:41:12 +0100

    adopting new blueprint schema

--- code/__init__.py
+++ code/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from code.login.views import login_blueprint
+
+from code.login import bp as blueprint_login
 
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("copernicus.cfg")
 
 
-app.register_blueprint(login_blueprint)
+app.register_blueprint(blueprint_login)
