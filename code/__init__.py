d1e5322018559dd46162088a35bf5f1e5e9ab64d
msapunov <matvey.sapunov@gmail.com>
2018-12-03 16:40:43 +0100

    Add LoginManager

--- code/__init__.py
+++ code/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 
 from code.login import bp as blueprint_login
 
@@ -6,5 +7,6 @@ from code.login import bp as blueprint_login
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("copernicus.cfg")
 
+login = LoginManager(app)
 
 app.register_blueprint(blueprint_login)
