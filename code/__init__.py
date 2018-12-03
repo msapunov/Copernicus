3005171376cb67a2942cc630b9a4d9cc5996a216
msapunov <matvey.sapunov@gmail.com>
2018-12-03 16:41:05 +0100

    Add blueprint for project page

--- code/__init__.py
+++ code/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 
 from code.login import bp as blueprint_login
+from code.project import bp as blueprint_project
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -10,3 +11,4 @@ app.config.from_pyfile("copernicus.cfg")
 login = LoginManager(app)
 
 app.register_blueprint(blueprint_login)
+app.register_blueprint(blueprint_project)
