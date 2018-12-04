6f1a9aff1fbf63635a462bb8a6af7bcc13d11ca6
msapunov <matvey.sapunov@gmail.com>
2018-12-04 16:34:24 +0100

    Add SQLAlchemy support

--- code/__init__.py
+++ code/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager
-
+from flask_sqlalchemy import SQLAlchemy
 from code.login import bp as blueprint_login
 from code.project import bp as blueprint_project
 from code.stat import bp as blueprint_stat
-
+from code.DB.schema import UserDB
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("copernicus.cfg")
+
+db = SQLAlchemy()
+db.init_app(app)
 
 login = LoginManager(app)
 login.login_view = "login.login"
