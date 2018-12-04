0b6916a1436b4f33975f2dbdb69caa899b8cd575
msapunov <matvey.sapunov@gmail.com>
2018-12-04 12:32:03 +0100

    Add stat blueprint

--- code/__init__.py
+++ code/__init__.py
@@ -3,13 +3,16 @@ from flask_login import LoginManager
 
 from code.login import bp as blueprint_login
 from code.project import bp as blueprint_project
+from code.stat import bp as blueprint_stat
 
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("copernicus.cfg")
 
 login = LoginManager(app)
-login.login_view = "login.html"
+login.login_view = "login.login"
+login.session_protection = "strong"
 
 app.register_blueprint(blueprint_login)
 app.register_blueprint(blueprint_project)
+app.register_blueprint(blueprint_stat)
