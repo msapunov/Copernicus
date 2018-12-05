b6c007eeb777a2076acb63bb213b6db4c24dbe1e
msapunov <matvey.sapunov@gmail.com>
2018-12-05 16:39:04 +0100

    Finalizing bits for login/logout logic

--- code/login/url.py
+++ code/login/url.py
@@ -1,9 +1,8 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, g
 from flask_login import current_user, login_user, logout_user, login_required
 from code.login.magic import ssh_login
 from code.login.form import LoginForm
 from code.login import bp
-
 
 from logging import warning
 
@@ -11,11 +10,14 @@ from logging import warning
 @bp.before_app_request
 def before_request():
     if current_user.is_authenticated:
-        pass
+        g.name = current_user
 
 
 @bp.route("/login.html", methods=["GET", "POST"])
 def login():
+
+    from code.database.schema import User
+
     if current_user.is_authenticated:
         return redirect(url_for("stat.index"))
     form = LoginForm(request.form)
@@ -26,18 +28,17 @@ def login():
     username = form.login.data
     password = form.passw.data
     if not ssh_login(username, password):
-        return False
-
-    from code.database.schema import User
+        return redirect(url_for("login.login"))
 
     user = User.query.filter_by(login=username).first()
     warning(user)
     login_user(user, True)
-    warning("RENDERING THE PAGE!")
+    g.name = username
     return redirect(url_for("stat.index"))
 
 
 @bp.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("login.login"))
