12c18fb9a85885f520338b9d6db7d71971235c1b
msapunov <matvey.sapunov@gmail.com>
2018-12-10 16:10:51 +0100

    Add cache to store list of users. The default expiration time is 10 minutes

--- code/__init__.py
+++ code/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, g
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.contrib.cache import SimpleCache
 from code.pages.login import bp as blueprint_login
 from code.pages.project import bp as blueprint_project
 from code.pages.stat import bp as blueprint_stat
@@ -9,6 +10,8 @@ from code.pages.admin import bp as blueprint_admin
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("copernicus.cfg")
+
+cache = SimpleCache()
 
 db = SQLAlchemy()
 db.init_app(app)
@@ -21,6 +24,18 @@ from code.database.schema import User
 @login.user_loader
 def load_user(userid):
     return User.query.filter(User.id == userid).first()
+
+@app.before_request
+def first_request():
+    user_list = cache.get("user_list")
+    if not user_list:
+        users_obj = User.query.all()
+        users = map(lambda x: x.login, users_obj)
+        user_list = sorted(list(users))
+        cache.set("user_list", user_list, 600)
+        print("*" * 80)
+    g.user_list = user_list
+
 
 app.register_blueprint(blueprint_login)
 app.register_blueprint(blueprint_project)
