c6305b4d063062ffa742bdc7e5ec9c8eeffdd61e
msapunov <matvey.sapunov@gmail.com>
2018-12-12 17:00:04 +0100

    url controller and function to handle switch user

---
+++ code/pages/admin/url.py
@@ -0,0 +1,27 @@
+from flask import g, flash, request, redirect, url_for, app
+from code.pages.admin import bp
+from flask_login import login_required, login_user
+
+
+@bp.route("/switch_user", methods=["POST"])
+@login_required
+def web_switch_user():
+    username = request.form.get("switch_user")
+
+    if not username in g.user_list:
+        flash("Invalid username: '%s'" % username)
+        flash(request.referrer)
+        print(request.referrer)
+        print(app)
+        if request.referrer:
+            return redirect(request.referrer)
+        else:
+            return redirect(url_for("stat.index"))
+
+    from code.database.schema import User
+
+    user = User.query.filter_by(login=username).first()
+    print(user)
+    login_user(user, True)
+    flash("Username: '%s'" % username)
+    return redirect(url_for("stat.index"))
