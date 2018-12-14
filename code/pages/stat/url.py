73d5bab4f742994830af390608c6c9754af69e83
msapunov <matvey.sapunov@gmail.com>
2018-12-14 11:20:46 +0100

    job related code incorporated to a function get_jobs

--- code/pages/stat/url.py
+++ code/pages/stat/url.py
@@ -8,7 +8,11 @@ from code.pages.stat.magic import ssh_wrapper
 @bp.route('/index', methods=["GET", "POST"])
 @login_required
 def index():
+    jobs = get_jobs()
+    data = {"jobs": jobs}
+    return render_template("stat.html", data=data)
 
+def get_jobs():
     cmd = ["sacct", "-nPX",
            "--format=JobID,State,Start,Account,JobName,CPUTime,Partition",
            "--start=02/15/18-00:00", "--end=12/13/18-15:35", "-u",
@@ -19,16 +23,18 @@ def index():
 
     if not result:
         flash("No jobs found")
-    data = {"jobs": []}
+    jobs = []
     for job in result:
         tmp = {}
         job = job.strip().split("|")
-        tmp["id"] = job[0]
+        tmp["id"] = int(job[0])
         tmp["project"] = job[3]
         tmp["state"] = job[1]
         tmp["partition"] = job[6]
         tmp["date"] = job[2]
         tmp["name"] = job[4]
         tmp["duration"] = job[5]
-        data["jobs"].append(tmp)
-    return render_template("stat.html", data=data)
+        jobs.append(tmp)
+    jobs = sorted(jobs, key=lambda x: x["id"], reverse=True)
+    result = jobs[0:10]
+    return result
