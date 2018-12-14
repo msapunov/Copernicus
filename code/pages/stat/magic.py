44e61e7a2f50a18e7036ec4c97b0063bfb7fc2cd
msapunov <matvey.sapunov@gmail.com>
2018-12-14 10:33:16 +0100

    ssh tunnel for command execution on remote host

---
+++ code/pages/stat/magic.py
@@ -0,0 +1,29 @@
+from paramiko import SSHClient, AutoAddPolicy, AuthenticationException, RSAKey
+from flask import current_app
+from logging import debug, error
+
+
+def ssh_wrapper(cmd):
+    debug("ssh_wrapper(%s)" % cmd)
+    host = current_app.config["SSH_SERVER"]
+    login = current_app.config["SSH_USERNAME"]
+    key_file = current_app.config["SSH_KEY"]
+    key = RSAKey.from_private_key_file(key_file)
+
+    debug("Connecting to %s with login %s and key %s" % (host, login, key_file))
+    client = SSHClient()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    try:
+        client.connect(host, username=login, pkey=key)
+    except AuthenticationException:
+        error("Failed to connect to %s under using %s with key '%s'"
+              % (host, login, key_file))
+    finally:
+        client.close()
+
+    stdin, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines()
+    errors = stderr.readlines()
+    client.close()
+
+    return output, errors
