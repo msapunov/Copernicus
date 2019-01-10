a6a97cee30a7b9e1d8b0dfe38dfb80b9f7080952
msapunov <matvey.sapunov@gmail.com>
2019-01-10 16:45:02 +0100

    remove useless printouts and old code

--- code/pages/project/url.py
+++ code/pages/project/url.py
@@ -12,8 +12,6 @@ def project_index():
     start = accounting_start()
     end = dt.now().strftime("%m/%d/%y-%H:%M")
     projects = get_project_info(start, end)
-    print(projects)
-
     data = {"projects": projects}
     return render_template("project.html", data=data)
 
@@ -45,28 +43,17 @@ def get_project_info(start, end):
         else:
             project["consumption"] = 0
             project["usage"] = 0
-    print(tmp)
 
-    """
-    projects = get_project_consumption(tmp, start, end)
-    result = []
-    for key in projects.keys():
-        projects[key]["name"] = key
-        if ("consumed" not in projects[key]) or (
-                "private" not in projects[key]):
-            continue
-        total = projects[key]["max"]
-        if total > 0:
-            for i in ["consumed", "private"]:
-                val = projects[key][i]
-                tmp_usage = "{0:.1%}".format(float(val) / float(total))
-                projects[key]["%s_use" % i] = float(tmp_usage.replace("%", ""))
-        else:
-            projects[key]["consumed_use"] = 0
-            projects[key]["private_use"] = 0
-        print(projects[key])
-        result.append(projects[key])
-    """
+        for user in project["users"]:
+            username = user["login"]
+            if name not in conso:
+                user["consumption"] = 0
+                continue
+            if username not in conso[name]:
+                user["consumption"] = 0
+                continue
+            user["consumption"] = conso[name][username]
+
     return tmp
 
 
@@ -79,6 +66,7 @@ def get_project_consumption(projects, start, end):
     result, err = ssh_wrapper(run)
     if not result:
         return flash("No project consumption information found")
+    print(result)
 
     tmp = {}
     for item in result:
@@ -92,45 +80,3 @@ def get_project_consumption(projects, start, end):
             tmp[project][user] = int(conso)
     print(tmp)
     return tmp
-
-
-def get_scratch():
-    cmd = "beegfs-ctl --getquota --csv --uid %s" % current_user.login
-    result, err = ssh_wrapper(cmd)
-    if not result:
-        return flash("No scratch space info found")
-
-    info = result[1]
-    name, uid, used, total, files, hard = info.split(",")
-    usage = "{0:.1%}".format(float(used) / float(total))
-    free = float(total) - float(used)
-    return {"usage": usage, "total": total, "used": used, "free": free,
-            "used_label": bytes2human(used), "free_label": bytes2human(free)}
-
-
-def get_jobs(start, end):
-    cmd = ["sacct", "-nPX",
-           "--format=JobID,State,Start,Account,JobName,CPUTime,Partition",
-           "--start=%s" % start, "--end=%s" % end, "-u",
-           current_user.login]
-    run = " ".join(cmd)
-
-    result, err = ssh_wrapper(run)
-
-    if not result:
-        return flash("No jobs found")
-    jobs = []
-    for job in result:
-        tmp = {}
-        job = job.strip().split("|")
-        tmp["id"] = job[0]
-        tmp["project"] = job[3]
-        tmp["state"] = job[1]
-        tmp["partition"] = job[6]
-        tmp["date"] = job[2]
-        tmp["name"] = job[4]
-        tmp["duration"] = job[5]
-        jobs.append(tmp)
-    jobs = sorted(jobs, key=lambda x: x["id"], reverse=True)
-    result = jobs[0:10]
-    return result
