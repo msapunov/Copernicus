9775fa52eac8db218ed3127e3eb9564aa51f7346
msapunov <matvey.sapunov@gmail.com>
2019-01-25 10:33:30 +0100

    methods to deal with requests acceptance and rejection

--- code/pages/board/url.py
+++ code/pages/board/url.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect, url_for, g, flash, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
+from code.pages import ProjectLog
+from code.pages import check_int, check_string
 from code.pages.board import bp
 
 
@@ -15,3 +17,50 @@ def web_board():
         return render_template("board.html", error=err)
     result = list(map(lambda x: x.to_dict(), ext_list))
     return render_template("board.html", data=result)
+
+
+@bp.route("/accept", methods=["POST"])
+@login_required
+def web_board_accept():
+    from code import db
+    record = board_action()
+    record.accepted = True
+    db.session.commit()
+    ProjectLog(record.project).accept(record)
+    return jsonify({"update": record})
+
+
+@bp.route("/reject", methods=["POST"])
+@login_required
+def web_board_reject():
+    from code import db
+    record = board_action()
+    record.accepted = False
+    db.session.commit()
+    ProjectLog(record.project).reject(record)
+    return jsonify({"update": record})
+
+
+def board_action():
+
+    from code.database.schema import Extend
+
+    data = request.get_json()
+    if not data:
+        return jsonify(message="Expecting application/json requests")
+    status, result = check_int(data["eid"])
+    if not status:
+        return result
+    eid = result
+
+    status, result = check_string(data["comment"])
+    if not status:
+        return result
+    note = result
+
+    extend = Extend().query.filter(Extend.id == eid).one()
+    if not extend:
+        return jsonify(message="No extension with id '%s' found" % eid)
+    extend.processed = True
+    extend.decision = note
+    return extend
