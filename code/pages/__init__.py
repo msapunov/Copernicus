489085c25823d383dc62d02f7121d61ec834f935
msapunov <matvey.sapunov@gmail.com>
2019-01-28 11:09:38 +0100

    move ssh_wrapper function to module init

--- code/pages/__init__.py
+++ code/pages/__init__.py
@@ -1,4 +1,32 @@
 from flask_login import current_user
+from paramiko import SSHClient, AutoAddPolicy, AuthenticationException, RSAKey
+from flask import current_app
+from logging import debug, error
+
+
+def ssh_wrapper(cmd):
+    debug("ssh_wrapper(%s)" % cmd)
+    host = current_app.config["SSH_SERVER"]
+    login = current_app.config["SSH_USERNAME"]
+    key_file = current_app.config["SSH_KEY"]
+    key = RSAKey.from_private_key_file(key_file)
+
+    debug("Connecting to %s with login %s and key %s" % (host, login, key_file))
+    client = SSHClient()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    try:
+        client.connect(host, username=login, pkey=key)
+    except AuthenticationException:
+        error("Failed to connect to %s under using %s with key '%s'"
+              % (host, login, key_file))
+        client.close()
+
+    stdin, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines()
+    errors = stderr.readlines()
+    client.close()
+
+    return output, errors
 
 
 def check_int(raw_int):
