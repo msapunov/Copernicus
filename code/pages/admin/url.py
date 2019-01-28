35c871d00a393222ea521d9170139e228577c6ae
msapunov <matvey.sapunov@gmail.com>
2019-01-28 15:37:56 +0100

    add handler for user info ("/admin/user/info")

--- code/pages/admin/url.py
+++ code/pages/admin/url.py
@@ -43,6 +43,24 @@ def web_admin_partition_info():
     return jsonify(data=partition)
 
 
+@bp.route("/admin/user/info", methods=["POST"])
+@login_required
+def web_admin_user_info():
+    result, err = ssh_wrapper("PROCPS_USERLEN=32 PROCPS_FROMLEN=90 w -s -h")
+    if not result:
+        raise ValueError("Error getting partition information: %s" % err)
+
+    users = []
+    for user in result:
+        output = user.split()
+        login = output[0].strip()
+        host = output[2].strip()
+        cmd = " ".join(output[4:]).strip()
+
+        users.append({"username": login, "from": host, "process": cmd})
+    return jsonify(data=users)
+
+
 @bp.route("/admin", methods=["GET", "POST"])
 @bp.route("/admin.html", methods=["GET", "POST"])
 @login_required
