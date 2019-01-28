e9db7c4060a0c659507e847ffbd452666f95634d
msapunov <matvey.sapunov@gmail.com>
2019-01-28 11:10:29 +0100

    parsing and returning partition info from ssh output

--- code/pages/admin/url.py
+++ code/pages/admin/url.py
@@ -1,6 +1,7 @@
-from flask import g, flash, request, redirect, url_for, render_template
-from code.pages.admin import bp
+from flask import g, flash, request, redirect, url_for, render_template, jsonify
 from flask_login import login_required, login_user
+from code.pages.admin import bp
+from code.pages import ssh_wrapper
 
 
 @bp.route("/switch_user", methods=["POST"])
@@ -20,6 +21,26 @@ def web_switch_user():
     login_user(user, True)
     flash("Username: '%s'" % username)
     return redirect(url_for("user.user_index"))
+
+
+@bp.route("/admin/partition/info", methods=["POST"])
+@login_required
+def web_admin_partition_info():
+    result, err = ssh_wrapper("sinfo -s")
+    if not result:
+        raise ValueError("Error getting partition information: %s" % err)
+
+    partition = []
+    for record in result:
+        if "PARTITION" in record:
+            continue
+        name, avail, time, nodes, nodelist = record.split()
+        name = name.strip()
+        nodes = nodes.strip()
+        allocated,idle,other,total = nodes.split("/")
+        partition.append({"partition": name, "allocated": allocated,
+                          "idle": idle, "other": other, "total": total})
+    return jsonify(data=partition)
 
 
 @bp.route("/admin", methods=["GET", "POST"])
