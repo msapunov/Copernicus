e721626edd80f331cb362a264f65769497444bc0
msapunov <matvey.sapunov@gmail.com>
2019-02-11 14:04:48 +0100

    == replacing "is"

--- code/pages/admin/url.py
+++ code/pages/admin/url.py
@@ -321,7 +321,7 @@ def web_admin():
     from code.database.schema import Register
 
     result = {"partition": slurm_partition_info()}
-    reg_list = Register().query.filter(Register.processed is False).all()
+    reg_list = Register().query.filter(Register.processed == False).all()
     if not reg_list:
         result["extension"] = False
     else:
