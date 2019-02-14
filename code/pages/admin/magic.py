7833c66a3b414d3bf78222421c753d0aa8833d62
msapunov <matvey.sapunov@gmail.com>
2019-02-14 14:40:21 +0100

    functions from admin/url.py

--- code/pages/admin/magic.py
+++ code/pages/admin/magic.py
@@ -39,3 +39,216 @@ def get_users(data):
             continue
         users.append(user)
     return users
+
+
+def accept_message(register, msg):
+    to = register.responsible_email
+    name = register.responsible_first_name
+    surname = register.responsible_last_name
+    mid = register.project_id()
+    title = "Your project request '%s' has been accepted" % mid
+    prefix = "Dear %s %s,\nYour project request '%s' has been accepted by" \
+             " scientific committee" % (name, surname, mid)
+    if msg:
+        msg = prefix + " with following comment:\n" + msg
+    else:
+        msg = prefix
+    return message(to, msg, title)
+
+
+def reject_message(register, msg):
+    to = register.responsible_email
+    name = register.responsible_first_name
+    surname = register.responsible_last_name
+    mid = register.project_id()
+    title = "Your project request '%s' has been rejected" % mid
+    prefix = "Dear %s %s,\nYour project request '%s' has been rejected with" \
+             " following comment:\n\n" % (name, surname, mid)
+    msg = prefix + msg
+    return message(to, msg, title)
+
+
+def message(to, msg, title=None):
+    by_who = current_app.config["EMAIL_PROJECT"]
+    cc = current_app.config["EMAIL_PROJECT"]
+    if not title:
+        title = "Concerning your project"
+    if not send_message(to, by_who, cc, title, msg):
+        return "Message was sent to %s successfully" % to
+
+
+def project_type(register):
+    if register.type_a:
+        return "a"
+    elif register.type_b:
+        return "b"
+    elif register.type_c:
+        return "c"
+    else:
+        raise ValueError("Failed to determine project's type")
+
+
+def project_assign_resources(register, approve):
+    from code.database.schema import Resources
+    resource = Resources(
+        approve=approve,
+        valid=True,
+        cpu=register.cpu,
+        type=project_type(register),
+        smp=register.smp,
+        gpu=register.gpu,
+        phi=register.phi
+    )
+    return resource
+
+
+def project_creation_magic(register, users, approve):
+    from code.database.schema import Project
+
+    project = Project(
+        title=register.title,
+        description=register.description,
+        scientific_fields=register.scientific_fields,
+        genci_committee=register.genci_committee,
+        numerical_methods=register.numerical_methods,
+        computing_resources=register.computing_resources,
+        project_management=register.project_management,
+        project_motivation=register.project_motivation,
+        active=True,
+        type=project_type(register),
+        approve=approve,
+        ref=register,
+        privileged=True if project_type is "h" else False,
+        responsible=users["responsible"],
+        users=users["users"]
+    )
+    return project
+
+
+def get_registration_record(pid):
+    from code.database.schema import Register
+
+    register = Register.query.filter_by(id=pid).first()
+    if not register:
+        raise ValueError("Project registration request with id %s not found"
+                         % pid)
+    return register
+
+
+def get_pid_notes(data):
+    pid = check_int(data["pid"])
+    note = check_str(data["note"])
+    debug("Got pid: %s and note: %s" % (pid, note))
+    return pid, note
+
+
+def is_user_exists(record):
+    from code.database.schema import User
+
+    name = record["name"] if "name" in record else False
+    surname = record["surname"] if "surname" in record else False
+    email = record["email"] if "email" in record else False
+    login = record["login"] if "login" in record else False
+
+    if login:
+        result = User.query.filter_by(login=login).first()
+    elif email:
+        result = User.query.filter_by(email=email).first()
+    elif name and surname:
+        result = User.query.filter_by(name=name, surname=surname).first()
+    else:
+        result = User.query.filter_by(login=login, email=email, name=name,
+                                      surname=surname).first()
+
+    print(result)
+    if result:
+        print(result.id)
+        record["exists"] = True
+    else:
+        record["exists"] = False
+    return record
+
+
+def get_uptime(server):
+    tmp = {}
+    result, err = ssh_wrapper("uptime", host=server)
+    if not result:
+        error("Error getting 'uptime' information: %s" % err)
+        return tmp
+
+    for up in result:
+        output = up.split(",")
+        for i in output:
+            if "users" in i:
+                users = i.replace("users", "")
+                users = users.strip()
+                try:
+                    users = int(users)
+                except Exception as err:
+                    error("Failed to convert to int: %s" % err)
+                    continue
+                tmp["users"] = users
+            if "load average" in i:
+                idx = output.index(i)
+                i = "|".join(output[idx:])
+                load = i.replace("load average: ", "")
+                load = load.strip()
+                loads = load.split("|")
+                tmp["load_1"] = loads[0]
+                tmp["load_5"] = loads[1]
+                tmp["load_15"] = loads[2]
+    return tmp
+
+
+def get_mem(server):
+    tmp = {}
+    result, err = ssh_wrapper("free -m", host=server)
+    if not result:
+        error("Error getting 'free' information: %s" % err)
+        return tmp
+
+    for mem in result:
+        output = mem.split(",")
+        for i in output:
+            if "total" in i:
+                continue
+            if "Mem" in i:
+                memory = i.split()
+                mem_total = int(memory[1].strip())
+                mem_available = int(memory[6].strip())
+                mem_used = mem_total - mem_available
+                mem_usage = "{0:.1%}".format(float(mem_used) / float(mem_total))
+                tmp["mem_total"] = mem_total
+                tmp["mem_avail"] = mem_available
+                tmp["mem_used"] = mem_used
+                tmp["mem_usage"] = mem_usage
+            if "Swap" in i:
+                swap = i.split()
+                swap_total = int(swap[1].strip())
+                swap_available = int(swap[3].strip())
+                swap_used = swap_total - swap_available
+                swap_usage = "{0:.1%}".format(float(swap_used) /
+                                              float(swap_total))
+                tmp["swap_total"] = swap_total
+                tmp["swap_avail"] = swap_available
+                tmp["swap_used"] = swap_used
+                tmp["swap_usage"] = swap_usage
+    return tmp
+
+
+def slurm_partition_info():
+    result, err = ssh_wrapper("sinfo -s")
+    if not result:
+        raise ValueError("Error getting partition information: %s" % err)
+
+    partition = []
+    for record in result:
+        if "PARTITION" in record:
+            continue
+        name, avail, time, nodes, nodelist = record.split()
+        name = name.strip()
+        nodes = nodes.strip()
+        allocated, idle, other, total = nodes.split("/")
+        partition.append({"name": name, "allocated": allocated, "idle": idle,
+                          "other": other, "total": int(total)})
+    return partition
