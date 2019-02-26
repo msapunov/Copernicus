c06c7dbd4359010ff644306cdcd333a32d31d28a
msapunov <matvey.sapunov@gmail.com>
2019-02-26 17:35:49 +0100

    add tonnes of non url handlers functions from url.py and adjust import accordingly

--- code/pages/project/magic.py
+++ code/pages/project/magic.py
@@ -1,8 +1,91 @@
-from flask import flash
+from flask import flash, current_app, jsonify, request
 from flask_login import current_user
-from code.pages import ssh_wrapper
+from code.pages import ssh_wrapper, check_int, check_str
 from code.utils import accounting_start
 from datetime import datetime as dt
+from code.pages import send_message
+
+
+def extend_update():
+    from code.database.schema import Extend, Project
+
+    data = request.get_json()
+    if not data:
+        raise ValueError("Expecting application/json requests")
+
+    if "project" in data:
+        pid = check_int(data["project"])
+    else:
+        raise ValueError("Project ID is missing")
+    if "cpu" in data:
+        cpu = check_int(data["cpu"])
+    else:
+        cpu = 0
+    if "note" in data:
+        note = check_str(data["note"])
+    else:
+        raise ValueError("Comment is absent")
+
+    project = Project().query.filter_by(id=pid).first()
+    if not project:
+        raise ValueError("Failed to find a project with id: %s" % pid)
+
+    p_name = project.get_name()
+    p_info = get_project_consumption([p_name])
+    if not p_info:
+        use = 0
+    else:
+        use = p_info[p_name]["total"]
+    maximum = project.resources.cpu
+    if not use:
+        usage = 0
+    else:
+        usage = "{0:.1%}".format(float(use)/float(maximum))
+    return Extend(project=project, hours=cpu, reason=note, present_use=use,
+                  usage_percent=usage, present_total=maximum)
+
+
+def send_extend_mail(project, extend):
+    subj = "Request extend project %s" % project.name
+    msg = """
+    Dear %s
+    You have requested to extend your project %s with %s hours
+    Extension reason is:
+      %s
+    Your request will be examined shortly
+    """ % (project.responsible.full_name(), project.name, extend.hours,
+           extend.reason)
+    project_email(project.responsible.email, subj, msg)
+
+
+def send_activate_mail(project, extend):
+    subj = "Request to activate project %s" % project.name
+    msg = """
+    Dear %s
+    You have requested to activate your suspended project %s
+    The reason for activation is:
+      %s
+    Your request will be examined shortly
+    """ % (project.responsible.full_name(), project.name, extend.reason)
+    project_email(project.responsible.email, subj, msg)
+
+
+def send_transform_mail(project, extend):
+    subj = "Request to transform project %s" % project.name
+    msg = """
+    Dear %s
+    You have requested to transform your project %s of type A to type B
+    The reason for transformation is:
+      %s
+    Your request will be examined shortly
+    """ % (project.responsible.full_name(), project.name, extend.reason)
+    project_email(project.responsible.email, subj, msg)
+
+
+def project_email(to, title, msg):
+    by_who = current_app.config["EMAIL_PROJECT"]
+    return jsonify(data=send_message(to, by_who=by_who, title=title,
+                                     message=msg))
 
 
 def get_project_info(start=None, end=None):
