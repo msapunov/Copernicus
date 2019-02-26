85e674636e13bc96f40b4d16d0c3393b1721df76
msapunov <matvey.sapunov@gmail.com>
2019-02-26 17:29:00 +0100

    initial commit of file with non url handlers functions

---
+++ code/pages/project/magic.py
@@ -0,0 +1,78 @@
+from flask import flash
+from flask_login import current_user
+from code.pages import ssh_wrapper
+from code.utils import accounting_start
+from datetime import datetime as dt
+
+
+def get_project_info(start=None, end=None):
+    from code.database.schema import Project
+
+    p_ids = current_user.project_ids()
+    tmp = []
+    for pid in p_ids:
+        project = Project().query.filter_by(id=pid).first()
+        if not project:
+            continue
+        if current_user != project.get_responsible():
+            continue
+        tmp.append(project.to_dict())
+    if not tmp:
+        return flash("No active projects found for user '%s'" %
+                     current_user.login)
+
+    tmp_project = list(map(lambda x: x["name"], tmp))
+    conso = get_project_consumption(tmp_project, start, end)
+    if not conso:
+        return tmp
+
+    for project in tmp:
+        name = project["name"]
+        if name in conso:
+            total = conso[name]["total"]
+            project["consumption"] = total
+            project["usage"] = "{0:.1%}".format(
+                float(total)/float(project["resources"]["cpu"]))
+        else:
+            project["consumption"] = 0
+            project["usage"] = 0
+
+        for user in project["users"]:
+            username = user["login"]
+            if name not in conso:
+                user["consumption"] = 0
+                continue
+            if username not in conso[name]:
+                user["consumption"] = 0
+                continue
+            user["consumption"] = conso[name][username]
+
+    return tmp
+
+
+def get_project_consumption(projects, start=None, end=None):
+    if not start:
+        start = accounting_start()
+    if not end:
+        end = dt.now().strftime("%m/%d/%y-%H:%M")
+
+    name = ",".join(projects)
+    cmd = ["sreport", "cluster", "AccountUtilizationByUser", "-t", "hours"]
+    cmd += ["-nP", "format=Account,Login,Used", "Accounts=%s" % name]
+    cmd += ["start=%s" % start, "end=%s" % end]
+    run = " ".join(cmd)
+    result, err = ssh_wrapper(run)
+    if not result:
+        return flash("No project consumption information found")
+
+    tmp = {}
+    for item in result:
+        item = item.strip()
+        project, user, conso = item.split("|")
+        if project not in tmp:
+            tmp[project] = {}
+        if not user:
+            tmp[project]["total"] = int(conso)
+        else:
+            tmp[project][user] = int(conso)
+    return tmp
