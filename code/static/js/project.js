99621059992fa6808a4fc559e4d3d4c2950b7445
msapunov <matvey.sapunov@gmail.com>
2019-03-20 18:19:00 +0100

    fixing title's grammar

--- code/static/js/project.js
+++ code/static/js/project.js
@@ -114,7 +114,7 @@ function reduce_to_names(initial, object){
     window.render.new_user = function(e){
         var name = $(this).data("name");
         var id = $(this).data("project");
-        var title = "Create new user for the project {0}?".f(name);
+        var title = "Add a new user to the project {0}?".f(name);
         var name = $("<input/>").addClass("uk-width-1-1").attr({
             "name": "name",
             "type": "text",
