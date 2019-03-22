23a6f5012ad3e72609e97f3fb26ebc2082ba0770
msapunov <matvey.sapunov@gmail.com>
2019-03-22 15:07:24 +0100

    strip and lower input variable before checking for string or not

--- code/pages/project/url.py
+++ code/pages/project/url.py
@@ -61,7 +61,7 @@ def web_project_delete_user():
     if not data:
         raise ValueError("Expecting application/json requests")
     pid = check_int(data["project"])
-    login = check_str(data["login"])
+    login = check_str(data["login"].strip().lower())
     project = get_project_record(pid)
     users = list(map(lambda x: x.login, project.users))
     if login not in users:
