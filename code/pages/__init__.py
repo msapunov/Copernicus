95ec38b6d599d4f062c1aa0d2ac888378ef17f61
msapunov <matvey.sapunov@gmail.com>
2019-04-19 15:20:23 +0200

    Add MAIL_SEND configuration option (True/False) which defines whether or not send an email

--- code/pages/__init__.py
+++ code/pages/__init__.py
@@ -56,7 +56,8 @@ def send_message(to_who, by_who=None, cc=None, title=None, message=None):
     postfix = "If this email has been sent to you by mistake, please report " \
               "to: %s" % tech
     msg.body = message + "\n" + postfix
-    mail.send(msg)
+    if current_app.config["MAIL_SEND"]:
+        mail.send(msg)
     return "Message was sent to %s successfully" % ", ".join(to_who)
 
 
