bb84bf3ca8ca72362bddd930879f8adf70a69fe9
msapunov <matvey.sapunov@gmail.com>
2019-06-06 16:24:51 +0200

    remove unused imports and big chunk of legacy code

--- base/pages/admin/url.py
+++ base/pages/admin/url.py
@@ -4,15 +4,9 @@ from flask_login import login_required, login_user
 from base.pages import ssh_wrapper, send_message, Task
 from base.pages.user.magic import get_user_record
 from base.pages.admin import bp
-from base.pages.admin.magic import remote_project_creation_magic, get_users
-from base.pages.admin.magic import get_responsible, get_registration_record
-from base.pages.admin.magic import is_user_exists, get_pid_notes, get_uptime
-from base.pages.admin.magic import slurm_partition_info, project_creation_magic
-from base.pages.admin.magic import project_assign_resources, get_mem, message
-from base.pages.admin.magic import accept_message, reject_message, TaskManager
-from base.pages.admin.magic import get_ltm, task_action
+from base.pages.admin.magic import get_uptime, get_mem, get_ltm, TaskManager
+from base.pages.admin.magic import slurm_partition_info
 from base.pages.project.magic import pending_resources, processed_resource
-from logging import info, debug
 
 
 __author__ = "Matvey Sapunov"
@@ -114,98 +108,6 @@ def admin_tasks_done(tid):
 
     return jsonify(data=Task(tid).done())
 
-
-"""
-@bp.route("/admin/registration/users", methods=["POST"])
-@login_required
-def web_admin_registration_users():
-
-    print(request.form)
-    pid = check_int(request.form.get("pid"))
-
-    register = get_registration_record(pid)
-    tmp_responsible = {
-        "name": register.responsible_first_name,
-        "surname": register.responsible_last_name,
-        "email": register.responsible_email
-    }
-    responsible = is_user_exists(tmp_responsible)
-    users = []
-    return jsonify(data={"responsible": responsible, "users": users})
-
-
-@bp.route("/admin/registration/accept", methods=["POST"])
-@login_required
-def web_admin_registration_accept():
-    from base.database.schema import Register, User
-    from base import db
-
-    approve = User.query.filter_by(login=login_user).first()
-    if not approve:
-        raise ValueError("Can't find user %s in database!" % login_user)
-    data = request.get_json()
-    if not data:
-        raise ValueError("Expecting application/json requests")
-    pid, note = get_pid_notes(data)
-    responsible = get_responsible(data)
-    users = {"responsible": responsible, "users": get_users(data)}
-    register = Register.query.filter_by(id=pid).first()
-    if not register:
-        raise ValueError("Project with id %s not found" % pid)
-    debug("Perform project creation")
-    project = project_creation_magic(register, users, approve)
-    db.session.add(project)
-    db.session.commit()  # get the id of the record which becomes the project's name
-    debug("Updating registration request DB")
-    register.accepted = True
-    register.processed = True
-    register.comment = note
-    p_resources = project_assign_resources(register, approve)
-    db.session.add(p_resources)
-    project_name = project.get_name()
-    gid = remote_project_creation_magic(project_name, users)
-    project.resources = p_resources
-    project.gid = gid
-    project.name = project_name
-    db.session.commit()
-    info("Project has been created successfully")
-    return jsonify(data=accept_message(register, note))
-
-
-@bp.route("/admin/registration/reject", methods=["POST"])
-@login_required
-def web_admin_registration_reject():
-    from base.database.schema import Register
-    from base import db
-
-    data = request.get_json()
-    if not data:
-        raise ValueError("Expecting application/json requests")
-    pid, note = get_pid_notes(data)
-    register = Register.query.filter_by(id=pid).first()
-    if not register:
-        raise ValueError("Project with id %s not found" % pid)
-    register.accepted = False
-    register.processed = True
-    register.comment = note
-    db.session.commit()
-    return jsonify(data=reject_message(register, note))
-
-
-@bp.route("/admin/message/register", methods=["POST"])
-@login_required
-def web_admin_message():
-    from base.database.schema import Register
-
-    data = request.get_json()
-    if not data:
-        raise ValueError("Expecting application/json requests")
-    pid, note = get_pid_notes(data)
-    register = Register.query.filter_by(id=pid).first()
-    if not register:
-        raise ValueError("Project with id %s not found" % pid)
-    return jsonify(data=message(register.responsible_email, note))
-"""
 
 @bp.route("/admin/partition/info", methods=["POST"])
 @login_required
