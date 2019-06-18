8adf563097d8ca70ccf4bedd881f5ed5c3dc188a
msapunov <matvey.sapunov@gmail.com>
2019-06-18 17:35:03 +0200

    First commit of a user edit form

---
+++ base/pages/admin/form.py
@@ -0,0 +1,18 @@
+from flask_wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
+
+
+class UserEditForm(Form):
+    login = StringField("Login", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    email = EmailField("Surname", validators=[DataRequired(), Email()])
+    active = BooleanField("Surname", default=True, validators=[DataRequired()])
+    is_user = BooleanField("User", default=True, validators=[DataRequired()])
+    is_responsible = BooleanField("Responsible", validators=[DataRequired()])
+    is_manager = BooleanField("Manager", validators=[DataRequired()])
+    is_tech = BooleanField("Tech", validators=[DataRequired()])
+    is_committee = BooleanField("Committee", validators=[DataRequired()])
+    is_admin = BooleanField("Admin", validators=[DataRequired()])
