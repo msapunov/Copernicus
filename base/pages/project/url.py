fb60911e6cede13b985afad279fb975bda870c15
msapunov <matvey.sapunov@gmail.com>
2019-06-24 11:08:39 +0200

    URL handler to display the list of projects

--- base/pages/project/url.py
+++ base/pages/project/url.py
@@ -4,7 +4,7 @@ from base.pages import ProjectLog, check_int, check_str, check_mail, TaskQueue
 from base.pages import generate_login
 from base.pages.user import bp
 from base.pages.project.magic import get_project_info, get_project_record
-from base.pages.project.magic import get_project_overview
+from base.pages.project.magic import get_project_overview, get_list_of_projects
 from base.pages.project.magic import extend_update, get_limbo_users, get_users
 from base.pages.user.magic import get_user_record
 from datetime import datetime as dt
@@ -13,6 +13,12 @@ from operator import attrgetter
 
 __author__ = "Matvey Sapunov"
 __copyright__ = "Aix Marseille University"
+
+
+@bp.route("/project/list", methods=["POST"])
+@login_required
+def project_list():
+    return jsonify(data=get_list_of_projects())
 
 
 @bp.route("/project/overview/annie", methods=["POST"])
