988b7bfe99f6f08948d4ba52b41e6410b8933bac
msapunov <matvey.sapunov@gmail.com>
2019-07-01 11:25:35 +0200

    Fix uwsgi startup error

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from base import create_app
 
+app = create_app("copernicus.cfg")
 
 if __name__ == "__main__":
-
-    app = create_app("copernicus.cfg")
     app.run(debug=True, use_debugger=False, use_reloader=False,
             passthrough_errors=True)
