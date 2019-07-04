826c3b0ce810c27415dc353d99e3773652077c2c
msapunov <matvey.sapunov@gmail.com>
2019-07-04 18:26:16 +0200

    Remove user_add handler since it's called in different way now

--- base/static/js/admin.js
+++ base/static/js/admin.js
@@ -746,7 +746,6 @@
     $(document).on("click", ".history_info", window.render.new_project);
     $(document).on("click", ".history_info", window.render.tasks_sel_update);
 
-    $(document).on("click", ".user_add", window.render.user_add);
     $(document).on("click", ".user_edit", window.render.user_add);
     $(document).on("click", ".user_purge", window.render.user_purge);
     $(document).on("click", ".user_block", window.render.user_block);
