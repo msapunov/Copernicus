8af5d1bc3128d2535a59019e34ff2f038724bc00
msapunov <matvey.sapunov@gmail.com>
2019-10-24 16:17:04 +0200

    User record is having an UID field

--- base/pages/user/url.py
+++ base/pages/user/url.py
@@ -82,6 +82,7 @@ def user_index():
             "name": user_record.name,
             "surname": user_record.surname,
             "email": user_record.email,
+            "uid": user_record.id,
             "login": user_record.login}
     try:
         jobs = get_jobs(start, end)
