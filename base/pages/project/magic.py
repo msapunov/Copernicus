8db0d8466b42d5ae8311501540154485f9a79be0
msapunov <matvey.sapunov@gmail.com>
2020-02-05 16:56:36 +0100

    set timestamp in filename of activity report pdf

--- base/pages/project/magic.py
+++ base/pages/project/magic.py
@@ -88,7 +88,8 @@ def save_activity(req):
 def save_report(data, project):
     project_name = project.get_name()
     html = render_template("report.html", data=data)
-    name = "%s_activity_report.pdf" % project_name
+    ts = dt.now().replace(microsecond=0).isoformat("_")
+    name = "%s_activity_report_%s.pdf" % (project_name, ts)
     path = str(Path(get_tmpdir(current_app), name))
     log.debug("The resulting PDF will be saved to: %s" % path)
     pdf = from_string(html, path)
