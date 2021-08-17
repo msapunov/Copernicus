24596bcc0e204ff7b5d520a61fedf13a8653ef0c
Matvey Sapunov <matvey.sapunov@univ-amu.fr>
2021-08-17 23:43:02 +0200

    user.edit function re-written for use modal system

--- base/static/js/user.js
+++ base/static/js/user.js
@@ -6,57 +6,12 @@
         edit: "user/edit"
     };
 
-    window.user.edit=function(){
-        var login = $.trim( $(this).data("login") );
-        var name = $.trim( $(this).data("name") );
-        var surname = $.trim( $(this).data("surname") );
-        var email = $.trim( $(this).data("email") );
-
-        var title = "Change person information for account '{0}'?".f(login);
-        var n_field = $("<input/>").addClass("uk-width-1-1").attr({
-            "name": "name",
-            "type": "text",
-            "value": name.capitalize()
-        });
-        var s_field = $("<input/>").addClass("uk-width-1-1").attr({
-            "name": "surname",
-            "type": "text",
-            "value": surname.capitalize()
-        });
-        var e_field = $("<input/>").addClass("uk-width-1-1").attr({
-            "name": "email",
-            "type": "email",
-            "value": email
-        });
-        var form = $("<form/>").addClass("uk-form").append(
-            $("<legend/>").text(title)
-        ).append(
-            $("<div/>").addClass("uk-form-row").append(n_field)
-        ).append(
-            $("<div/>").addClass("uk-form-row").append(s_field)
-        ).append(
-            $("<div/>").addClass("uk-form-row").append(e_field)
-        ).append(
-            $("<div/>").text("Please double check if provided data are correct").attr({"id":"psa"}).addClass("uk-form-row uk-alert")
-        );
-        var popup = dialog(form.prop("outerHTML"), function(){
-            var data = {
-                "name": $("input[name=name]").val(),
-                "surname": $("input[name=surname]").val(),
-                "email": $("input[name=email]").val(),
-                "login": login
-            };
-            if( ! data_check(data) ){
-                $("#psa").text("Please, complete the field(s) highlighted in red!").addClass("uk-alert-danger");
-                return false;
-            }
-            json_send(window.user.url, data).done(function(reply){
-                if(reply.data){
-                    UIkit.notify(reply.data, {timeout: 2000, status:"success"});
-                }
-                popup.hide();
-            });
-        });
+    window.user.edit=function(e){
+        var modal = $.trim( $(this).data("modal") );
+        var form = $.trim( $(this).data("form") );
+        var data = $("#" + form).serialize()
+        ajax_send(window.user.url.edit, data, modal);
+        e.preventDefault();
     }
 
     $(document).on("ready", function(){
