9dfeb8b78580819ce0befccbe3fe037094747e57
Matvey Sapunov <matvey.sapunov@univ-amu.fr>
2021-08-22 16:37:34 +0200

    Add custom filter to jinja template

--- base/pages/project/__init__.py
+++ base/pages/project/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 
 bp = Blueprint("project", __name__)
+@bp.app_template_filter()
+def datetime_format(value, representation="%H:%M %d-%m-%y"):
+    if not value:
+        return None
+    return value.strftime(representation)
 
 from base.pages.project import url
