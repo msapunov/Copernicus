4f50ffea889655942a1d10434f359bde969e71c8
Matvey Sapunov <matvey.sapunov@univ-amu.fr>
2021-08-28 16:40:02 +0200

    Remove unused code

--- base/pages/admin/magic.py
+++ base/pages/admin/magic.py
@@ -136,36 +136,6 @@ def remote_project_creation_magic(name, users):
     return True
 
 
-def get_responsible(data):
-    from base.database.schema import User
-
-    if not data.responsible_id:
-        raise ValueError("Responsible's ID is missing")
-    rid = check_int(data["responsible_id"])
-
-    resp = User.query.filter_by(User.id == rid).first()
-    if not resp:
-        raise ValueError("No responsible's record in DB with id: %s" % rid)
-    return resp
-
-
-def get_users(data):
-    from base.database.schema import User
-
-    if not data.responsible_id:
-        raise ValueError("Responsible's ID is missing")
-    uids = map(lambda x: check_int(x), data["users"])
-
-    users = []
-    for uid in uids:
-        user = User.query.filter_by(User.id == uid).first()
-        if not user:
-            error("No responsible's record in DB with id: %s" % uid)
-            continue
-        users.append(user)
-    return users
-
-
 def accept_message(register, msg):
     to = register.responsible_email
     name = register.responsible_first_name
