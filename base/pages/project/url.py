1f7939c7479487c2430894b033a1544ef1f1b831
Matvey Sapunov <matvey.sapunov@univ-amu.fr>
2021-08-29 16:02:38 +0200

    project_activity now is using WTForm for request parsing

--- base/pages/project/url.py
+++ base/pages/project/url.py
@@ -69,7 +69,8 @@ def project_activity_upload():
 @bp.route("/project/activity/<string:project_name>", methods=["POST"])
 @login_required
 def project_activity(project_name):
-    return jsonify(message=report_activity(project_name, request))
+    form = ActivityForm()
+    return jsonify(message=report_activity(project_name, form))
 
 
 @bp.route("/project/info/<string:project_name>", methods=["POST"])
