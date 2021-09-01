15da3b69ba8db12124baa747a29d1ba24b768f4d
MS <matvey.sapunov@gmail.com>
2021-09-01 10:39:05 +0200

    Removed bunch of code cause some if it in lib now and there is only one URL to be used

--- base/static/js/user.js
+++ base/static/js/user.js
@@ -1,27 +1,15 @@
 (function(window, document, $, undefined){
     "use strict";
-    window.user = {};
-    window.user.url = {
-        modal: "user/modal/edit",
-        edit: "user/edit"
-    };
-
-    window.user.edit=function(e){
-        var modal = $.trim( $(this).data("modal") );
-        var form = $.trim( $(this).data("form") );
-        var data = $("#" + form).serialize()
-        ajax_send(window.user.url.edit, data, modal);
-        e.preventDefault();
-    }
+    let url = "user/modal/edit";
 
     $(document).on("ready", function(){
         var login = $.trim( $("#user_login").data("login") );
-        modal("{0}/{1}".f(window.user.url.modal, login), "edit");
+        modal("{0}/{1}".f(url, login), "edit");
     });
 
     $(document).on("click", ".window_hide", trigger_modal);
     $(document).on("click", ".edit", trigger_modal);
-    $(document).on("click", ".edit_submit", window.user.edit);
+    $(document).on("click", ".edit_submit", submit);
     $(document).on({
         mouseenter: function () {
             $(".edit").toggleClass("uk-hidden");
