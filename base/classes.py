bd06350e09e0c67f8535bc610d681924158ac80e
MS <matvey.sapunov@gmail.com>
2021-09-29 06:18:54 +0200

    Add accept method to pending class

--- base/classes.py
+++ base/classes.py
@@ -472,6 +472,15 @@ class Pending:
         self.commit()
         return list(result)
 
+    def accept(self):
+        """
+        :return: List. List of processed register records
+        """
+        self.action = "accept"
+        result = map(lambda x: self.process_record(x), self.pending)
+        self.commit()
+        return list(result)
+
     def process_record(self, record):
         """
         Set processed field of the task record to True, so the task will be
