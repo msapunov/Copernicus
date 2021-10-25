d3ea0775743a7f382ad656aa6ef88716d5cdad9f
MS <matvey.sapunov@gmail.com>
2021-10-25 09:15:26 +0200

    Remove reg_approve import

--- base/pages/admin/url.py
+++ base/pages/admin/url.py
@@ -31,8 +31,7 @@ from base.pages.admin.magic import (
     get_registration_record,
     space_info,
     reg_reject,
-    reg_accept,
-    reg_approve)
+    reg_accept)
 from base.functions import slurm_nodes_status
 from base.pages.admin.form import (
     PendingActionForm,
