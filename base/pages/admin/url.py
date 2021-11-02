4c69fb7ce5e9880e259d5964147c1f16d8b9e6f6
MS <matvey.sapunov@gmail.com>
2021-11-02 23:10:12 +0100

    bugfix: Using brief method for TaskDB record otherwise the record is not serializable

--- base/pages/admin/url.py
+++ base/pages/admin/url.py
@@ -397,7 +397,7 @@ def web_admin_tasks_list():
 @login_required
 @grant_access("admin", "tech")
 def admin_tasks_done(tid):
-    return jsonify(data=process_task(tid))
+    return jsonify(data=process_task(tid).brief())  # brief method of TaskDB rec
 
 
 @bp.route("/admin/partition/info", methods=["POST"])
