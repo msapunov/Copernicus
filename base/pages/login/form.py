f55ccb1d53a22938124dabb20c3aafd35f47d64b
MS <matvey.sapunov@gmail.com>
2021-11-24 16:38:56 +0100

    Create MessageForm class which will be used for sending messages via emails and add TextAreaField import

--- base/pages/login/form.py
+++ base/pages/login/form.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired
+
+
+class MessageForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired(
+        message="Message field is empty")])
 
 
 class LoginForm(FlaskForm):
