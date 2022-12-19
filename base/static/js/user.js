bb30678afa6a9281f9f887cbcc02fae13c86642c
msapunov <matvey.sapunov@gmail.com>
2022-12-19 15:07:45 +0100

    Add support for new layout and Upload SSH key button

--- base/static/js/user.js
+++ base/static/js/user.js
@@ -1,23 +1,19 @@
 (function(window, document, $, undefined){
     "use strict";
     let url = "user/modal/edit";
+    let ssh = "user/modal/ssh";
 
     $(document).on("ready", function(){
         var login = $.trim( $("#user_login").data("login") );
         modal("{0}/{1}".f(url, login), "edit");
+        modal(ssh, "ssh");
     });
 
     $(document).on("click", ".window_hide", trigger_modal);
     $(document).on("click", ".edit", trigger_modal);
+    $(document).on("click", ".ssh", trigger_modal);
     $(document).on("click", ".edit_submit", submit);
-    $(document).on({
-        mouseenter: function () {
-            $(".edit").toggleClass("uk-hidden");
-        },
-        mouseleave: function () {
-            $(".edit").toggleClass("uk-hidden");
-        }
-    }, ".user_info");
+    $(document).on("click", ".ssh_submit", submit);
     $(document).on("hide.uk.modal", form_reset);
 
 })(window, document, jQuery);
