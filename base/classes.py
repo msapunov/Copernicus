dc9e5b5551037d8be90f9d430e046bc5b3c556f5
msapunov <matvey.sapunov@gmail.com>
2023-01-25 16:04:45 +0100

    Add password_changed method to UserLog class

--- base/classes.py
+++ base/classes.py
@@ -299,6 +299,10 @@ class UserLog(Log):
         self.log.event = "Upload SSH key %s" % key
         return self.commit()
 
+    def password_changed(self, passwd):
+        self.log.event = "Password has been changed"
+        return self.commit()
+
     def password_reset(self, passwd):
         self.log.event = "Password has been reset"
         return self.commit(Mail().user_password(self, passwd))
