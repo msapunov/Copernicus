c79db6b39a6de5a1b8bf2f6c8368ad97b99afffd
msapunov <matvey.sapunov@gmail.com>
2023-01-25 17:15:01 +0100

    Import generate_password function which will be used in reset_password method

--- base/database/schema.py
+++ base/database/schema.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from base import db
+from base.functions import generate_password
 from datetime import datetime as dt
 from textwrap import shorten
 from hashlib import md5
