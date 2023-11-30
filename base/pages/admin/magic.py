dcd3b2739de56a71b2a948808f2428d5fb610721
msapunov <matvey.sapunov@gmail.com>
2023-11-30 23:17:32 +0100

    Set value to load_data var

--- base/pages/admin/magic.py
+++ base/pages/admin/magic.py
@@ -716,6 +716,8 @@ def get_server_info(server):
     for i in result:
         if "load average" in i:
             uptime_data = i
+        if "Load" in i:
+            load_data = i.replace("Load:", "").strip()
         elif "Mem" in i:
             memory_data = i
         elif "Swap" in i:
