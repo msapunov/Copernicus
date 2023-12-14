f3ce898112a99856b259d6d8be6184fbc7ddf833
msapunov <matvey.sapunov@gmail.com>
2023-12-14 07:44:05 +0100

    Replace user_consumption by logically more appropriate consumption_by_user method which can group resource consumption not only by user but by dates too

--- base/database/schema.py
+++ base/database/schema.py
@@ -122,33 +122,14 @@ class Project(db.Model):
     def __repr__(self):
         return '<Project {}>'.format(self.get_name())
 
-    def account_by_user(self, user=None):
-        result = self.resources.group_by_user()
+    def account_by_user(self, daily=None):
+        result = self.resources.consumption_by_user(daily)
         if not result:
             return {}
-        return {key: value for key, value in result}
-
-    def user_account(self, user=current_user):
-        result = self.resources.user_consumption(user)
-        return result if result else 0
-
-    def account_group(self, daily=None):
-        query = (Accounting.query.join(User, Accounting.user_id == User.id)
-                 .filter(Accounting.resources_id == self.resources_id))
         if daily:
-            result = query.group_by(
-                User.login, Accounting.date
-            ).with_entities(
-                User.login, Accounting.date, func.sum(Accounting.cpu)
-            ).all()
             return {i[1].strftime("%Y-%m-%d"): {i[0]: i[2]} for i in result}
         else:
-            result = query.group_by(
-                User.login
-            ).with_entities(
-                User.login, func.sum(Accounting.cpu)
-            ).all()
-            return {i[0]: i[1] for i in result }
+            return {key: value for key, value in result}
 
     def account(self):
         result = self.resources.consumption()
