d87e37f4ddd81b60735ca448f994ee04b4edef69
msapunov <matvey.sapunov@gmail.com>
2024-04-25 00:26:19 +0200

    Quotes and doublequotes changed according to rules

--- base/email.py
+++ base/email.py
@@ -47,7 +47,7 @@ class Mail(Thread):
                              % path)
         with open(path, "rb") as fd:
             part = MIMEApplication(fd.read(), Name=str(attach_file.name))
-        part['Content-Disposition'] = 'attachment; filename="%s"' % str(attach_file.name)
+        part["Content-Disposition"] = "attachment; filename='%s'" % str(attach_file.name)
         self.msg.attach(part)
         debug("File %s attached" % path)
         return self
