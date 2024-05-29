c64adbaea0646f37832ee62522fc802d9fc91057
msapunov <matvey.sapunov@gmail.com>
2024-05-29 11:15:36 +0200

    Add tasks url as root

--- base/static/js/admin.js
+++ base/static/js/admin.js
@@ -12,6 +12,7 @@
         system: "admin/sys/info",
         pending: "admin/pending/list",
         accounting: "admin/accounting/365",
+        task: "admin/tasks",
         tasks: "admin/tasks/list",
         tasks_accept: "admin/tasks/accept",
         tasks_history: "admin/tasks/history",
