47f46533196ef7592f3798c49819e569acaca800
msapunov <matvey.sapunov@gmail.com>
2024-05-29 11:24:16 +0200

    Change in task_show listener which calls window.admin.task_show  instead of showing prerendered html

--- base/static/js/admin.js
+++ base/static/js/admin.js
@@ -355,8 +355,8 @@
         $(document).on("click", ".pending_reject", trigger_modal);
         $(document).on("click", ".pending_reject_submit", window.admin.submit);
 
-        $(document).on("click", ".task_show", trigger_modal);
         $(document).on("click", ".task_list", trigger_modal);
+        $(document).on("click", ".task_show", window.admin.task_show);
         $(document).on("click", ".task_accept_submit", window.admin.task_submit);
         $(document).on("click", ".task_ignore_submit", window.admin.task_submit);
         $(document).on("click", ".task_reject_submit", window.admin.task_submit);
