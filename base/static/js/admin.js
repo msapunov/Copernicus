1b4144f1d8429153e07d70181b17185a7367940e
msapunov <matvey.sapunov@gmail.com>
2024-06-04 18:45:57 +0200

    changes in text and in function call

--- base/static/js/admin.js
+++ base/static/js/admin.js
@@ -50,11 +50,6 @@
         if(task === ''){
             return show_error({'status': 505, 'responseText': 'Failed to find task description for this record'});
         }
-        let url = "{0}/{1}/{2}".f(window.admin.url.task, act, id);
-        let message = "{0} task: {1}?<br>Task:<br>{2}".f(act.capitalize(), id, task);
-        if(act == 'reject'){
-            return UIkit.modal.prompt(message, "Task '{0}' is rejected".f(task), function(reply_text){
-                ajax(url, reply_text, this);
         let url = '{0}/{1}/{2}'.f(window.admin.url.task, act, id);
 //        let message = "{0} task: {1}?<br>{2}".f(act.capitalize(), id, task);
         let message = '{0} {1} ?'.f(act.capitalize(), task);
