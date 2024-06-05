63b32ac966d02f5d12dc70a801f3535134a62c49
msapunov <matvey.sapunov@gmail.com>
2024-06-05 18:19:29 +0200

    Double quotes removed by single quotes

--- base/static/js/admin.js
+++ base/static/js/admin.js
@@ -392,6 +392,7 @@
                 $("#task_queue_length").text(reply.data.length);
                 if($.fn.DataTable.isDataTable('#tasks_table')){
                     $('#tasks_table').DataTable().clear().rows.add(reply.data).draw();
+                $('#task_queue_length').text(reply.data.length);
                 }
             }
         });
