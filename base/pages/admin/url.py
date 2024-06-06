b0196cb543358bee7fbc30435df4fb4d9ca4cc4c
msapunov <matvey.sapunov@gmail.com>
2024-06-06 18:39:11 +0200

    Import of state info function from admin.magic

--- base/pages/admin/url.py
+++ base/pages/admin/url.py
@@ -16,7 +16,7 @@ from base.pages.admin.magic import (
     all_users,
     event_log,
     get_server_info,
-    get_ltm,
+    slurm_state_info,
     TaskManager,
     slurm_partition_info,
     process_task,
