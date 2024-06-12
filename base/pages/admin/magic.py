5f25ae0b9b36c1a468874e21bc5ca592fb246861
msapunov <matvey.sapunov@gmail.com>
2024-06-12 14:17:57 +0200

    Add date command to the sets of commands to execute on login server

--- base/pages/admin/magic.py
+++ base/pages/admin/magic.py
@@ -665,7 +665,7 @@ def get_server_info(server):
     out = {"server": server, "uptime": "", "memory": "", "load": "", "swap": ""}
     cmd = "echo cores:`nproc` && uptime -p && free -b | grep -v total && uptime"
     cmd += "| awk '/average/ {OFS=\":\"; print \"Load\",$(NF-2),$(NF-1),$NF}'"
-    cmd += "&& who | cut -d' ' -f1 | sort -u"
+    cmd += "&& who | cut -d' ' -f1 | sort -u && echo Time: `date +%s`"
     result, err = ssh_wrapper(cmd, host=server)
     if not result:
         error("Error getting information from the remote server: %s" % err)
