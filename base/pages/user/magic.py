c341639be96012301a154eafec62fa6576e2b2e3
msapunov <matvey.sapunov@gmail.com>
2025-02-03 19:13:49 +0100

    Quickfix for empty records

--- base/pages/user/magic.py
+++ base/pages/user/magic.py
@@ -23,7 +23,7 @@ def archive_check():
     logins = raw_data.decode("utf-8", errors="replace").split("\n")
     users = (
         User.query.filter(User.login.not_in(logins))
-        .filter(User.archived.isnot(None))
+        .filter(User.archived.is_(None))
         .with_for_update()
         .all()
     )
