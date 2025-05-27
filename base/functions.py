07c7126797ce645de035959a6bdd91e72ba82027
msapunov <matvey.sapunov@gmail.com>
2025-05-27 18:06:26 +0200

    Remove get_tmpdir function. It's the application method now

--- base/functions.py
+++ base/functions.py
@@ -475,67 +475,6 @@ def file_as_string(name):
         return b64encode(img_file.read()).decode("ascii")
 
 
-def get_tmpdir(application):
-    """
-    Check if application specific directory has been already created and create
-    said directory if it doesn't exists. If directory started with prefix is
-    already there the function returns first element from the directory list
-    :param application: Current flask application
-    :return: String. Name of the temporary application specific directory.
-    """
-    prefix = get_tmpdir_prefix(application)
-    dirs = [x[0] for x in walk(gettempdir())]
-    exists = list(filter(lambda x: True if prefix in x else False, dirs))
-    if exists:
-        dir_name = exists[0]
-        debug("Found existing directory: %s" % dir_name)
-    else:
-        dir_name = mkdtemp(prefix=prefix)
-        debug("Temporary directory created: %s" % dir_name)
-    return dir_name
-
-
-def get_tmpdir_prefix(application):
-    """
-    Construct the prefix for the temporary directory based on SECRET_KEY
-    parameter from configuration file
-    :param application: Current application
-    :return: String
-    """
-    return "%s_copernicus_" % application.config.get("SECRET_KEY", "XXX")[0:3]
-
-
-def save_file(req, directory, file_name=False):
-    """
-    Saving incoming file HTTP request to provided directory under original or
-    provided filename
-    :param req: Incoming file HTTP request
-    :param directory: Directory where to save the file from incoming request
-    :param file_name: Default is False. By default the file is saved under name
-    extracted from file http request.
-    :return: Dictionary
-    """
-    if "file" not in req.files:
-        raise ValueError("File expected!")
-    file = req.files["file"]
-    if file.filename == '':
-        raise ValueError("No selected file")
-    debug("File name from incoming request: %s" % file.filename)
-    if not file_name:
-        file_name = file.filename
-    else:
-        if "." not in file_name and "." not in file.filename:
-            file_name = "%s.unknown" % file_name
-        elif "." not in file_name and "." in file.filename:
-            ext = file.filename.rsplit('.', 1)[1].lower()
-            debug("Deducted file extensions: %s" % ext)
-            file_name = "%s.%s" % (file_name, ext)
-    name = join_dir(directory, file_name)
-    debug("Saving file from incoming request to: %s" % name)
-    file.save(name)
-    return {"saved_name": file_name, "incoming_name": file.filename}
-
-
 """
 Bytes-to-human / human-to-bytes converter.
 Based on: https://goo.gl/kTQMs
