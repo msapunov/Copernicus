0ee0da74f5921f03378139c74e3070b807938e48
msapunov <matvey.sapunov@gmail.com>
2025-06-06 14:32:31 +0200

    Remove check for user.active and blindly activate it

--- base/classes.py
+++ base/classes.py
@@ -958,11 +958,11 @@ class Task:
         user = self.task.user
         if user not in project.users:
             project.users.append(user)
-        if not user.active:
-            user.active = True
-            UserMailingList().add(user.email, user.full_name())
-            if user.acl.is_responsible:
-                ResponsibleMailingList().add(user.email, user.full_name())
+        user.archived = None
+        user.active = True
+        UserMailingList().add(user.email, user.full_name())
+        if user.acl.is_responsible:
+            ResponsibleMailingList().add(user.email, user.full_name())
         return ProjectLog(project).user_activated(self.task)
 
     def user_assign(self):
