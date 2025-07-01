dfe669ff4bca774015a563863b15d3fb8916d2b0
msapunov <matvey.sapunov@gmail.com>
2025-07-01 18:24:34 +0200

    remove config option from function call

--- base/pages/project/magic.py
+++ base/pages/project/magic.py
@@ -111,8 +111,8 @@ def sanity_check():
     cfg = g.project_config
     projects = db.session.query(Project).all()
     active_users_check(projects)
-    suspend_expired_projects(projects, cfg)
     warn_expired_projects(projects, cfg)
+    suspend_expired_projects(projects)
     suspend_overconsumed_projects(projects)
     warn_overconsumed_projects(projects)
     consumption_check(projects)
