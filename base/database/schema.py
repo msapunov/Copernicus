5b27507116e386c8d7b8a969bd271d7b02295168
msapunov <matvey.sapunov@gmail.com>
2025-10-23 15:43:14 +0200

    creation time set with lambda function

--- base/database/schema.py
+++ base/database/schema.py
@@ -867,7 +867,7 @@ class Tasks(db.Model):
     result = db.Column(db.Text, default=None)
     comment = db.Column(db.String, default=None)
 
-    created = db.Column(db.DateTime(True), server_default=func.now())
+    created = db.Column(db.DateTime(True), default=lambda: dt.now(timezone.utc), nullable=False)
     modified = db.Column(db.DateTime(True))
 
     uid = db.Column(db.Integer, db.ForeignKey("users.id"))
