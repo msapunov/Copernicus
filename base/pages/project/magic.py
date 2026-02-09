3ce0984b34209cb38dc3e640533d92636afa7864
msapunov <matvey.sapunov@gmail.com>
2026-02-09 09:33:44 +0100

    Add get_reservation_option function which set reservation property according to config file

--- base/pages/project/magic.py
+++ base/pages/project/magic.py
@@ -638,6 +638,24 @@ def is_project_renewable(project):
     return project
 
 
+def get_reservation_options(project):
+    """
+    Checking if reservation options is True.
+    :param project: String. Type of the project (i.e. subsection in project
+                         configuration file.
+    :return: List. List of tuples, first item in tuple is type to which this
+             project type can be transformed, second item is type's
+             description
+    """
+    cfg = g.project_config
+    ptype = project.type
+    if ptype in cfg:
+        project.reservation = cfg[ptype].get("reservation", None)
+    else:
+        project.reservation = None
+    return project
+
+
 def get_transformation_options(project_type=None):
     """
     Checking what transformation options are available in configuration file for
