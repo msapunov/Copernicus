10d0bd6b5cbd8a2f97dfdb77c8366d990d0247e3
msapunov <matvey.sapunov@gmail.com>
2026-02-25 18:05:11 +0100

    Add debug statement

--- base/functions.py
+++ base/functions.py
@@ -351,7 +351,6 @@ def get_finish(project):
     finish = parse_moment(finish_raw)
     if not finish:
         return None
-    debug(f"Got configuration value for renewal option: {update_raw}")
     update_raw = cfg.get("renew_start", None)
     debug(f"Got configuration value for renew_start option: {update_raw}")
     if not update_raw:
