50d6170bce90d390aa604225798affe9eb02b5ad
msapunov <matvey.sapunov@gmail.com>
2026-03-06 11:08:18 +0100

    Classical bug, can't strip on None

--- base/functions.py
+++ base/functions.py
@@ -374,10 +374,10 @@ def get_finish(project):
 def get_duration(project):
     cfg = g.project_config
     duration = cfg.get(project.type, {}).get("duration", None)
-    duration = duration.strip("'")
     debug(f"Got value '{duration}' for duration from config for {project}")
     if not duration:
         return None
+    duration = duration.strip("'")
     m = match(r"(\d+)\s*(day|week|month|year)s?", duration)
     if not m:
         return None
