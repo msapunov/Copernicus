d17127d24c33b061101890c4fecb7b3e76d610e1
msapunov <matvey.sapunov@gmail.com>
2026-09-01 13:00:08 +0200

    template task_expand_row.html works with dict now, not with record representation

--- base/pages/admin/magic.py
+++ base/pages/admin/magic.py
@@ -182,7 +182,7 @@ def render_registry(user):
 
 
 def render_task(task):
-    row = render_template("bits/task_expand_row.html", task=task)
+    row = render_template("bits/task_expand_row.html", task=task.to_dict())
     row += render_template("modals/tasks_accept_task.html", task=task.to_dict())
     row += render_template("modals/tasks_ignore_task.html", task=task.to_dict())
     row += render_template("modals/tasks_reject_task.html", task=task.to_dict())
