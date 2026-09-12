fd8fee32467adb78e135c03fdb4c12492c39f2ab
msapunov <matvey.sapunov@gmail.com>
2026-09-12 10:15:26 +0200

    Sorting imports

--- base/extensions.py
+++ base/extensions.py
@@ -4,11 +4,10 @@ Extensions module. Each extension is initialized in the app factory located in
 __init__.py
 """
 
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
+from flask_login import LoginManager
 from flask_mail import Mail
-
+from flask_sqlalchemy import SQLAlchemy
 
 mail = Mail()
 login_manager = LoginManager()
