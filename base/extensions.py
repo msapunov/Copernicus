3b90216ac273b2db9dfa7936511cf2a963b6a95d
msapunov <matvey.sapunov@gmail.com>
2026-09-12 10:16:00 +0200

    Add per-extension description

--- base/extensions.py
+++ base/extensions.py
@@ -10,6 +10,13 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 mail = Mail()
+"""Flask-Mail extension instance for sending emails."""
+
 login_manager = LoginManager()
+"""Flask-Login extension instance for user session management."""
+
 db = SQLAlchemy()
+"""Flask-SQLAlchemy extension instance for database access."""
+
 cache = Cache()
+"""Flask-Caching extension instance for caching data."""
