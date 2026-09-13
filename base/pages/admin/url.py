b16a97185a8fb013de1f9807816efe63f770b428
msapunov <matvey.sapunov@gmail.com>
2026-09-13 12:07:08 +0200

    Linter warning

--- base/pages/admin/url.py
+++ base/pages/admin/url.py
@@ -621,9 +621,12 @@ def web_admin_tasks_ignore(tid):
     task = Task(tid).ignore()
     tasks = TaskManager().list()
     if "admin.html" in request.referrer:
-        return jsonify(data=tasks, info="Task '%s' is ignored" % task.short(),
-                       task=True, html=render_template(
-            "modals/admin_show_task.html", data={"tasks": tasks}))
+        return jsonify(
+            data=tasks,
+            info="Task '%s' is ignored" % task.short(),
+            task=True,
+            html=render_template("modals/admin_show_task.html", data={"tasks": tasks}),
+        )
     return jsonify(data=tasks, task=True)
 
 
