d6ff4b9dc3e4ff24c905cdadc31aa0c8702fe94e
msapunov <matvey.sapunov@gmail.com>
2026-09-23 14:43:22 +0200

    Debug is false by default so removing it

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ from base import create_app
 app = create_app("copernicus.cfg")
 
 if __name__ == "__main__":
-    app.run(debug=True, use_debugger=False, use_reloader=False,
-            passthrough_errors=True)
+    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
